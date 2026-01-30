3fadb1408b47b497d21e8139b82969986ee88cc4
GoldGhost007 <146106713+GoldGhost007@users.noreply.github.com>
2026-01-29 19:37:42 -0800

    Initial commit of root build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+plugins {
+    kotlin("android") version "1.8.0"
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
